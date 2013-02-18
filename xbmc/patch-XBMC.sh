c287279f519f080744406e93f05e90b8d51e77de
Koenkk <koenkanters94@gmail.com>
2013-02-18 15:23:10 +0100

    Changed XBMC patch system

---
+++ xbmc/patch-XBMC.sh
@@ -0,0 +1,56 @@
+#
+#Copyright 2012 Koen Kanters
+#This file is part of XBian - XBMC on the Raspberry Pi.
+#
+#XBian is free software: you can redistribute it and/or modify it under the 
+#terms of the GNU General Public License as published by the Free Software 
+#Foundation, either version 3 of the License, or (at your option) any later 
+#version.
+#
+#XBian is distributed in the hope that it will be useful, but WITHOUT ANY 
+#WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+#FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+#details.
+#
+#You should have received a copy of the GNU General Public License along 
+#with XBian. If not, see <http://www.gnu.org/licenses/>
+#
+PATCHES=""
+if [ "$1" = "12" ]; then
+    PATCHES="TPNno.patch      
+    EGLRes.patch
+    XBianSysSum.patch
+    NetworkCachingRedux.patch
+    RemoveGUISoundSettings.patch
+    WOL.patch
+    Splash.patch
+    omxcodecs.patch
+    AvoidBlocking.patch
+    FixASSSubtitles.patch
+    DualAudioOutput.patch
+    remote.xml.patch
+    XBianConfluence.patch
+    Lircmap.xml.patch"
+elif [ "$1" = "13" ]; then
+    PATCHES="TPNno.patch
+    EGLRes.patch
+    XBianSysSum.patch
+    NetworkCachingRedux.patch
+    RemoveGUISoundSettings.patch
+    WOL.patch
+    Splash.patch
+    DualAudioOutput.patch
+    remote.xml.patch
+    XBianConfluence.patch
+    Lircmap.xml.patch"
+fi
+
+for patch in $PATCHES
+do
+    wget https://github.com/xbianonpi/xbian-patches/tree/master/xbmc/$patch -O $patch
+    patch -p1 < $patch
+    rm $patch
+done
+
+wget https://raw.github.com/xbianonpi/xbian/master/usr/local/share/xbmc/media/Splash.png
+mv Splash.png media/
