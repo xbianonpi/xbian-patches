ab7f7717175e1ff63ea4fffadf4c83426c5c083e
Koenkk <koenkanters94@gmail.com>
2013-04-25 10:29:59 +0200

    DualAudioOutput12.patch is broken

--- xbmc/patch-XBMC.sh
+++ xbmc/patch-XBMC.sh
@@ -17,12 +17,11 @@
 #
 # Patches that are currently not working (need an upgrade) :
 # XBMC13: eGalaxTouchScreen.patch, DualAudioOutput13.patch
-# XBMC12: eGalaxTouchScreen.patch
+# XBMC12: eGalaxTouchScreen.patch, DualAudioOutput12.patch
 
 PATCHES=""
 if [ "$1" = "12" ]; then
 PATCHES="TPNno.patch
-        DualAudioOutput12.patch
         NetworkCachingRedux12.patch
         EGLRes.patch
         XBianSysSum.patch
