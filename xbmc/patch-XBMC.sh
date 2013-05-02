d6616c95122d2e32efa8600872f99e8f4e162b70
Koenkk <koenkanters94@gmail.com>
2013-05-02 21:13:20 +0200

    [XBMC 12] Attempt to fix DualAudioOutput12.patch

--- xbmc/patch-XBMC.sh
+++ xbmc/patch-XBMC.sh
@@ -17,13 +17,14 @@
 #
 # Patches that are currently not working (need an upgrade) :
 # XBMC13: eGalaxTouchScreen.patch, DualAudioOutput13.patch, RemoveGUISoundSettings.patch, WOL13.patch
-# XBMC12: eGalaxTouchScreen.patch, DualAudioOutput12.patch
+# XBMC12: eGalaxTouchScreen.patch
 
 PATCHES=""
 if [ "$1" = "12" ]; then
 PATCHES="TPNno.patch
         NetworkCachingRedux12.patch
         EGLRes.patch
+        DualAudioOutput12.patch
         XBianSysSum.patch
         RemoveGUISoundSettings.patch
         Wiimote.patch
