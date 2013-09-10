4e844cf261c8591db4bb546b762ced0ad7df6d0a
Koenkk <koenkanters94@gmail.com>
2013-09-10 10:50:39 +0200

    Re-enable Splash.patch

--- xbmc/patch-XBMC.sh
+++ xbmc/patch-XBMC.sh
@@ -16,7 +16,7 @@
 #with XBian. If not, see <http://www.gnu.org/licenses/>
 #
 # Patches that are currently not working (need an upgrade) :
-# XBMC13: eGalaxTouchscreen.patch, DualAudioOutput.patch, Splash.patch
+# XBMC13: eGalaxTouchscreen.patch, DualAudioOutput.patch
 
 if [ "$1" = "12" ]; then
 PATCHES="Build12.patch
@@ -33,6 +33,7 @@ elif [ "$1" = "13" ]; then
 PATCHES="Build13.patch
         EGLRes.patch
         XBianSysSum.patch
+        Splash.patch
         Wiimote.patch
         RemoveGUISoundSettings13.patch
         XBianConfluence13.patch
